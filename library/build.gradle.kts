18591a5792a1f9e7c21f1da174f313cc27937001
Toly Pochkin <mr.toly@gmail.com>
2026-01-26 19:29:39 -0500

    bump up version

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 group = "dev.goquick.sqlitenow"
-version = "0.6.0-SNAPSHOT"
+version = "0.6.0"
 
 val isPublishingToMavenLocal =
     gradle.startParameter.taskNames.any { it.contains("publishToMavenLocal", ignoreCase = true) }
